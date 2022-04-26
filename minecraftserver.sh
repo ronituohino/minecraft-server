0716aba7a46a664bc05d0593c94a244407a60f18
Roni Tuohino <tuohinoroni@gmail.com>
2022-04-26 20:45:07 +0300

    Update minecraftserver.sh

--- minecraftserver.sh
+++ minecraftserver.sh
@@ -2,9 +2,15 @@
 apt-get update && apt -y upgrade
 apt install openjdk-17-jdk
 apt install screen 
+
 java -jar forge-1.18.2-40.1.0-installer.jar --installServer
 rm forge-1.18.2-40.1.0-installer.jar
+
 source ./run.sh
+
 sed -i 's/eula=false/eula=true/g' eula.txt
 unzip mods.zip -d mods
-source ./run.sh
+
+echo '-Xms2048M' >> user_jvm_args.txt
+echo '-Xmx4096M' >> user_jvm_args.txt
+echo 'nogui' >> user_jvm_args.txt
