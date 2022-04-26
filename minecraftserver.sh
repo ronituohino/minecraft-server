e1b2d6381e736a4f822413896495ddd5e3f25782
Roni Tuohino <tuohinoroni@gmail.com>
2022-04-26 19:08:34 +0300

    Update minecraftserver.sh

--- minecraftserver.sh
+++ minecraftserver.sh
@@ -3,3 +3,7 @@ apt-get update && apt -y upgrade
 apt install default-jre
 apt install screen 
 java -jar forge-1.18.2-40.1.0-installer.jar --installServer
+rm forge-1.18.2-40.1.0-installer.jar
+./run.sh
+sed -i 's/eula=false/eula=true/g' eula.txt
+./run.sh
