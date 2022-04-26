17de56f821cdd4d8d320c0d50413b4caad4ce07b
Roni Tuohino <tuohinoroni@gmail.com>
2022-04-26 19:02:13 +0300

    Update minecraftserver.sh

--- minecraftserver.sh
+++ minecraftserver.sh
@@ -2,4 +2,4 @@
 apt-get update && apt -y upgrade
 apt install default-jre
 apt install screen 
-java -jar ./server/forge-1.18.2-40.1.0.installer.jar --installServer
+java -jar forge-1.18.2-40.1.0.installer.jar --installServer
