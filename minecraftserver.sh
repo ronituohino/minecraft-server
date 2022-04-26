a9d2f1c7b670d47f7fdfe9e5b1833cf7a2a850fd
Roni Tuohino <tuohinoroni@gmail.com>
2022-04-26 19:00:52 +0300

    Move installer

--- minecraftserver.sh
+++ minecraftserver.sh
@@ -2,5 +2,4 @@
 apt-get update && apt -y upgrade
 apt install default-jre
 apt install screen 
-mkdir server
-unzip ./Ronin_rakennuspeli_1.18.2
+java -jar ./server/forge-1.18.2-40.1.0.installer.jar --installServer
